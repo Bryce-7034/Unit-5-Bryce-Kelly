c42af4c53cf06d513a2e0b192f5f2523a80488de
Bryce-7034 <brkelly6585@gmail.com>
2020-01-14 09:54:14 -0800

    egg

--- src/Slogan.java
+++ src/Slogan.java
@@ -1,6 +1,8 @@
-public class Slogan {
+public class Slogan implements Lockable{
     private static int instance;
     private String phrase;
+    private int key;
+    private boolean locked;
 
     public Slogan(){
         instance++;
@@ -21,5 +23,28 @@ public class Slogan {
         result += phrase;
 
         return result;
+    }
+    public void setKey(int key){
+        this.key = key;
+    }
+
+    public void unlock(int key){
+        if(key == this.key){
+            locked = false;
+        }
+        else
+            System.out.println("NO");
+    }
+
+    public void lock(int key){
+        if(key == this.key){
+            locked = true;
+        }
+        else
+            System.out.println("NO");
+    }
+
+    public boolean locked(){
+        return locked;
     }
 }
--- src/SloganTester.java
+++ src/SloganTester.java
@@ -1,21 +1,26 @@
-public class SloganTester {
+public class SloganTester{
     public static void main(String[]args) {
-        Slogan hello = new Slogan("hi");
-        System.out.println(Slogan.getInstances());
-        Slogan bye = new Slogan("Good Bye");
-        System.out.println(Slogan.getInstances());
-        Slogan empty = new Slogan();
-        System.out.println(Slogan.getInstances());
-        Slogan pie = new Slogan("3.141592653589793238462643383279502884197169399375105820974944592307816406286");
-        System.out.println(Slogan.getInstances());
+        Slogan.setKey(1);
+        Slogan.unlock(1);
+        while (Slogan.locked()){
+            Slogan hello = new Slogan("hi");
+            System.out.println(Slogan.getInstances());
+            Slogan bye = new Slogan("Good Bye");
+            System.out.println(Slogan.getInstances());
+            Slogan empty = new Slogan();
+            System.out.println(Slogan.getInstances());
+            Slogan pie = new Slogan("3.141592653589793238462643383279502884197169399375105820974944592307816406286");
+            System.out.println(Slogan.getInstances());
 
-        Slogan[] random = new Slogan[4];
-        random[0] = hello;
-        random[1] = bye;
-        random[2] = empty;
-        random[3] = pie;
+            Slogan[] random = new Slogan[4];
+            random[0] = hello;
+            random[1] = bye;
+            random[2] = empty;
+            random[3] = pie;
 
-        System.out.print(random[(int)(Math.random()*4)].toString());
-        //System.out.print(hello.toString()+"\n"+bye.toString()+"\n"+empty.toString()+"\n"+pie.toString());
+            System.out.print(random[(int) (Math.random() * 4)].toString());
+            //System.out.print(hello.toString()+"\n"+bye.toString()+"\n"+empty.toString()+"\n"+pie.toString());
+        }
+
     }
 }
